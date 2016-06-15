e7bc56f39c66015abfe4f02d1b7f1bd29977fe34
Homma Takuma <sfprhythnn@gmail.com>
2016-06-15 13:55:38 +0900

    エクササイズ前に戻す

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -3,5 +3,12 @@
 # You can add backtrace silencers for libraries that you're using but don't wish to see in your backtraces.
 # Rails.backtrace_cleaner.add_silencer { |line| line =~ /my_noisy_library/ }
 
+
+# 無視しても良いエラーの記述
+# rvm(rubyのバージョン管理)関連でのエラーをシカト
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /rvm/ }
+
+
+
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!
