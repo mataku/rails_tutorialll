1c45c016f7d1245edb922a96cce5dd56cf04f246
Homma Takuma <sfprhythnn@gmail.com>
2016-06-15 15:13:08 +0900

    minitestとguardを学nnda

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-# require 'minitest/reporters'
+require 'minitest/reporters'
 
-# Minitest::Reporters.use!
+Minitest::Reporters.use!
 
 
 class ActiveSupport::TestCase
