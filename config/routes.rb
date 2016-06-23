a9d74d49213590a6ea41a0b1c2f2a3beb169e439
Homma Takuma <sfprhythnn@gmail.com>
2016-06-23 18:48:30 +0900

    Listing 8.21までdone

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-
-  get 'sessions/new'
-
   root 'static_pages#home'
 
   # get 'test' => 'hobby/about'
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -11,6 +11,11 @@ require 'test_helper'
 # 6. new pageにてflashによるメッセージが表示されないのを確認
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -21,4 +26,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+  test "login with valid information" do
+    get login_path
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
 end
