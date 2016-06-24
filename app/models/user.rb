022ede72dd6adaa82e990b66785b84a4ba1f7440
Homma Takuma <sfprhythnn@gmail.com>
2016-06-24 14:49:18 +0900

    exercise1における、selfを用いた場合の表記が正しく書かれているかのテスト用
    This reverts commit 984059d227edf5d60728aa88206901f84be3bd1a.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,20 +9,19 @@ class User < ActiveRecord::Base
   has_secure_password
   validates(:password, presence: true, length: {minimum: 6} )
 
-  class << self
-    # Returns tha hash digest of the given string.
-    # def User.digest(string)
-    def digest(string)
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-      BCrypt::Password.create(string, cost: cost)
-    end
-
-    # Returns a random token.
-    # def User.new_token
-    def new_token
-      SecureRandom.urlsafe_base64
-    end
+  # Returns tha hash digest of the given string.
+  # def User.digest(string)
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
   end
+
+  # Returns a random token.
+  # def User.new_token
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
   # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
