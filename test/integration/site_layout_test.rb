c9183072c3ebcd535a210694bc01a8c43bac0321
Homma Takuma <sfprhythnn@gmail.com>
2016-06-28 12:12:56 +0900

    exercises用

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,6 +4,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -11,5 +15,30 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+  end
+
+  # ログインユーザーにおける、ページレイアウトテスト
+  test "layout links for logged-in users" do
+    log_in_as(@user)
+    get root_path
+    # ログインしているので、"Log in"ページはない
+    assert_select "a[href=?]", login_path, count: 0
+    
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+  end
+
+  # ログインしていないユーザーにおける、ページレイアウトテスト
+  test "layout links for non-logged-in users" do
+    get root_path
+    assert_select "a[href=?]", login_path
+
+    # ログインしていないので、ログインユーザー用のページはない
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", users_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", edit_user_path(@user), count: 0
   end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -8,9 +8,16 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
+  # friendly forwarding
+  # ログインが必要なページ(A)に未ログイン状態でアクセスした場合に、
+  # ログイン画面に遷移させて、ログインした後に、Aのページヘ戻す
+
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
+    assert_not_nil session[:forwarding_url]
+    assert_equal session[:forwarding_url], edit_user_url(@user)
     log_in_as(@user)
+    assert_nil session[:forwarding_url]
     assert_redirected_to edit_user_path(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
@@ -19,10 +26,13 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                     password: "",
                                     password_confirmation: "" }
     assert_not flash.empty?
+
     assert_redirected_to @user
+
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+
   end
 
   test "unsuccessful edit" do
