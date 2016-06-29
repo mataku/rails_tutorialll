cd976ad28b3a12fd743b3d733244d4c8e35036e8
Homma Takuma <sfprhythnn@gmail.com>
2016-06-29 11:59:35 +0900

    users_edit_test.rbにおける、session[:forwarding_url]のチェックの修正 (おぎどーのレビュー:pray:)

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -14,7 +14,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_not_nil session[:forwarding_url]
     assert_equal session[:forwarding_url], edit_user_url(@user)
     log_in_as(@user)
     assert_redirected_to edit_user_path(@user)
