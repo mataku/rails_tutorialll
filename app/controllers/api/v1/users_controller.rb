d2d843cf390da7ed1e61a8264b5de4a591c81841
Homma Takuma <sfprhythnn@gmail.com>
2016-07-13 17:30:53 +0900

    users_controllerにおけるindex, showアクション

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,16 @@
+require "#{Rails.root}/app/controllers/application_controller.rb"
+
+module Api
+  module V1
+    class UsersController < ApplicationController
+
+      def index
+        render json: User.all
+      end
+
+      def show
+        render json: User.find(params[:id])
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,14 @@ Rails.application.routes.draw do
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
 
+  # /api/v1/...
+  namespace :api, format: 'json' do
+    namespace :v1 do
+      resources :users
+    end
+    resources :users
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
