75087438e110b3554580f56324ffa74b809cc683
Homma Takuma <sfprhythnn@gmail.com>
2016-07-14 17:56:21 +0900

    update, destroyアクション

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    # ページごとにユーザーを表示する
     @users = User.paginate(page: params[:page])
     render json: @users
   end
@@ -20,6 +21,28 @@ class Api::V1::UsersController < ApplicationController
       render nothing: true, status: 200
     else
       render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @user = User.find_by(params[:id])
+
+    if @user.nil?
+      render json: {status: 404, content: {message: "No such user!"}} # :not_found
+    elsif @user.update_attributes(user_params)
+      render json: {status: 200, content: {message: "Updated!"}} # :ok
+    else
+      render json: {status: 422, content: {message: ""}} # :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find_by(params[:id])
+    if @user.nil?
+      render json: {status: :unprocessable_entity, content: {message: "No such user!"}}
+    else
+      @user.destroy
+      render json: {content: {message: "Deleted!"}}
     end
   end
 
