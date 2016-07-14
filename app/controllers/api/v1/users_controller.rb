3afa48e8afec4c5cdf84e4b2c4507d7a30cf9811
Homma Takuma <sfprhythnn@gmail.com>
2016-07-14 14:07:14 +0900

    user作成できたぞ!

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,33 @@
-require "#{Rails.root}/app/controllers/application_controller.rb"
 
-module Api
-  module V1
-    class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
 
-      def index
-        render json: User.all
-      end
+  # CSRFの認証用 (設定を後で考える)
+  skip_before_action :verify_authenticity_token
 
-      def show
-        render json: User.find(params[:id])
-      end
+  def index
+    @users = User.paginate(page: params[:page])
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      render nothing: true, status: 200
+    else
+      render json: @user.errors
     end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
